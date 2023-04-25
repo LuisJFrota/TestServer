06847a94cd54b6346eb438ea0d97e19162c512ce
Zoyd01 <klein.boelter@bluelabellabs.com>
2023-04-25 15:32:09 -0300

    Test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Server online");
+    res.json("Api running")
 })
 
-app.listen(3333);
+app.listen(3333 , () => 
+{
+    console.log("api running");
+});
