1e5d49b969fb3c6a52576e56749f8446db8f6db5
Zoyd01 <klein.boelter@bluelabellabs.com>
2023-04-25 14:48:15 -0300

    new config

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-const http = require('http');
-const process = require('process')
+const express = require('express')
 
-const hostname = '127.0.0.1';
-const port = process.env.port || 3000;
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Server Online!');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get('/', (req, res) => {
+    res.send("Server online");
 })
+
+app.listen(3333);
